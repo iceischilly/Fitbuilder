ae211a737f09007d539b6712fedb0fba4134027a
Ati14 <ati.gobin@student.tdsb.on.ca>
2024-05-28 15:17:55 -0400

    started code
    base code

---
+++ code.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+public class code { 
+    int end = 0;
+    int build = 0;
+    int edit = 0;
+    while (end !=1){
+        //start menu
+        // if else statement for starting game or closing game
+
+        //if statment to start
+        build = 1;
+        while (build != 0){
+            //insert code for building
+
+
+
+            //if statment for edit
+            edit = 1;
+        }
+        while (edit != 0) {
+            //code for edit
+            
+
+            //add if statment to exit
+            end = 1;
+            edit = 1;
+        }
+
+
+
+
+
+    }
+
+    
+}
